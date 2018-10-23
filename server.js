2572d808f29e01b33aad6642de0f1c022dec249e
shalom belaineh <shalom.belaineh@gmail.com>
2018-10-23 10:34:59 -0700

    server pulls location data

--- server.js
+++ server.js
@@ -13,28 +13,26 @@ const app = express();
 
 app.use(cors());
 
-
-app.get('/location', (request,response) => {
-  const locationData = searchToLatLong(request.query.data);
-  response.send(locationData);
+app.get ('/' ,(request,response)=> {
+    response.send('server is on');
+})
+app.get ('/location', (request,response)=> {
+    const locationData = searchToLatLong(request.query.data);
+    response.send(locationData);
 });
 
 function searchToLatLong(query) {
-  // Go to google (tomorrow)
-  const geoData = require('./data/geo.json');
-  const location = new Location(geoData.results[0]);
-	location.search_query = query;
-  return location;
+    const geoData = require('./geo.json');
+    const location = new Location(geoData.results[0]);
+        location.search_query =query;
+        return location;
 }
 
-function Location(data) { 
-  this.formatted_query = data.formatted_address;
-  this.latitude = data.geometry.location.lat;
-  this.longitude = data.geometry.location.lng;
+function Location(data) {
+    this.formatted_query = data.formatted_address;
+    this.latitude = data.geometry.location.lat;
+    this.longitude = data.geometry.location.lng;
+
 }
 
-
-
-app.listen(PORT, () => console.log(`App is up on ${PORT}`) );
-
-
+app.listen(PORT, ()=> console.log(`App is up on ${PORT}`));
